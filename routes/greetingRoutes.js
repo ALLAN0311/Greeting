24e9c1a510a093fe5bd6647b4b032170296d8757
AllanWinata <allan.winata@cakrawala.ac.id>
2025-10-01 11:45:26 +0700

    Update greetingRoutes.js

--- routes/greetingRoutes.js
+++ routes/greetingRoutes.js
@@ -1,8 +1,6 @@
-// routes/greetingRoutes.js
 const express = require('express');
 const router = express.Router();
 
-// GET /greeting?name=Teo
 router.get('/', (req, res) => {
   const name = req.query.name;
   if (name) return res.send(`hello ${name}`);
