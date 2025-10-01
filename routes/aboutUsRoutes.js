4f39846d35bc8dccdd89ac96b074fff83d932e9c
AllanWinata <allan.winata@cakrawala.ac.id>
2025-10-01 11:44:43 +0700

    aboutUsRoutes.js

--- routes/aboutUsRoutes.js
+++ routes/aboutUsRoutes.js
@@ -1,23 +1,18 @@
-// routes/aboutUsRoutes.js
 const express = require('express');
 const router = express.Router();
 
-// simple "database"
 const users = {
-  angling: { name: 'Angling Samarkhandi', nim: '24120300019' },
-  haikal:  { name: 'Muhammad Haikal',    nim: '24120300018' },
-  allan:   { name: 'Allan Ardi Winata',  nim: '24110500025' }
+    'Cecep': { name: 'Cecep', nim: '12345' },
+    'Ujang': { name: 'Ujang', nim: '67890' }
 };
 
-// GET /aboutus/:user
-router.get('/:user', (req, res) => {
-  const key = req.params.user.toLowerCase();
-  const found = users[key];
-  if (found) {
-    return res.json(found);
-  } else {
-    return res.status(404).json({ error: 'User not found' });
-  }
+router.get('/aboutus/:user', (req, res) => {
+    const user = req.params.user;
+    if (users[user]) {
+        res.json(users[user]);
+    } else {
+        res.status(404).send('Pengguna tidak ditemukan');
+    }
 });
 
 module.exports = router;
